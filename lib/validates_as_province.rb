ec852a6a109d1b03b13496f68bea7f9feb63ac2c
Paul Schreiber <paulschreiber@gmail.com>
2010-09-07 16:08:10 -0400

    first commit

---
+++ lib/validates_as_province.rb
@@ -0,0 +1,37 @@
+module ActiveRecord
+  module Validations
+    module ClassMethods
+      require "carmen"
+
+      def validates_as_province(*args)        
+        configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid],
+                          :on => :save, :with => nil
+                        }
+        configuration.update(args.pop) if args.last.is_a?(Hash)
+
+        validates_each(args, configuration) do |record, attr_name, value|
+          if configuration[:country].is_a?(String)
+            country = configuration[:country]
+          elsif configuration[:country].is_a?(Symbol) and record.respond_to?(configuration[:country])
+            country = record.send(configuration[:country])
+          elsif record.respond_to?(:country)
+            country = record.send(:country)
+          else
+            country = false
+          end
+          
+          next unless country          
+          current_province_list = Carmen::state_codes(country)
+          next unless current_province_list
+
+          value ||= ''
+
+          unless (configuration[:allow_blank] && value.blank?) || current_province_list.include?(value)
+            record.errors.add(attr_name, configuration[:message])
+          end
+        end
+      end # validates_as_province
+
+    end    
+  end
+end
